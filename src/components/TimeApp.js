f10be25305bfb9ba70013858bc0a304f65f4b5e5
Ramazan Doğan <doganrmzn40@gmail.com>
2022-12-04 22:47:12 +0300

    Time is ready
    The time is ready with using useState and js, function

--- src/components/TimeApp.js
+++ src/components/TimeApp.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useState } from 'react';
 
 const TimeApp = () => {
+   const cTime = new Date().toLocaleDateString();
+   const [time, setTime] = useState(cTime);
+
+   const updateTime = () => {
+      const newTime = new Date().toLocaleDateString();
+      setTime(newTime);
+   };
+
+   useEffect(() => {
+      const timeInterval = setInterval(updateTime, 1000);
+      return () => {
+         clearInterval(timeInterval);
+      };
+   }, [time]);
+
    return (
       <React.Fragment>
          <section>
-            <div className=" bg-red-300 hover:bg-slate-500 justify-center flex text-2xl h-10 ">
-               Ramlek
+            <div className=" flex justify-center items-center text-indigo-200 bg-fuchsia-800 w-96 h-96 mt-20 mx-auto rounded-full">
+               <div>
+                  <div>
+                     <p>Day</p>
+                  </div>
+                  <div>
+                     <p>{time}</p>
+                  </div>
+                  <div>
+                     <p>Date</p>
+                  </div>
+               </div>
             </div>
          </section>
       </React.Fragment>
