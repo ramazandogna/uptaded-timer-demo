d5f271331d07ba60b3070de223afa8b222203ef0
Ramazan Doğan <doganrmzn40@gmail.com>
2022-12-04 22:18:45 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+   return (
+      <div>
+         <h1>its 👉🏻 "Uptaded Timer Demo"</h1>
+         <h2>Hello Mr. 🎅🏻 and Mrs. 🤶🏻</h2>
+      </div>
+   );
+}
+
+export default App;
